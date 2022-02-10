c7dac1d2a54da9efcc034d5249e519010096544a
KaishAli <360mohdkaishali.cs@gmail.com>
2022-02-10 18:37:05 +0530

    Updates

---
+++ precache-manifest.bfd09b47727c0153038bdecf28eb3a48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5df06ea4453fcda63160",
+    "url": "/card/static/css/main.dfaffcf7.chunk.css"
+  },
+  {
+    "revision": "5df06ea4453fcda63160",
+    "url": "/card/static/js/main.5df06ea4.chunk.js"
+  },
+  {
+    "revision": "0d12850659dab149eedc",
+    "url": "/card/static/js/1.0d128506.chunk.js"
+  },
+  {
+    "revision": "ec3a1b962e1eca1a4002",
+    "url": "/card/static/js/runtime~main.ec3a1b96.js"
+  },
+  {
+    "revision": "50ef6ede6e7b1a7e176519de7574b628",
+    "url": "/card/index.html"
+  }
+];
